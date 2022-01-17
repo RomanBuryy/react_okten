77547698958b240a2d310068e0ba4b8784902c67
RomanBuryy <roman.buriy@gmail.com>
2022-01-17 21:15:33 +0200

    add lesson_2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    const send = (e) =>{
+        console.log(e)
+        e.preventDefault();
+
+
+    }
+
+
+
+    return (
+        <div className="App">
+            <form onSubmit={send}>
+                <div>
+                    <label>Name: <input type='text' name={'name'}/></label>
+                </div>
+                <button>Send</button>
+            </form>
+
+        </div>
+    );
 }
 
 export default App;
