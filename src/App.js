2d1cc896560f36ceb0b31a3c92bedb6af916e299
RomanBuryy <roman.buriy@gmail.com>
2022-01-19 19:18:26 +0200

    add lesson_2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
+import {useEffect, useState} from "react";
+
+import Users from "./components/Users/Users";
 import './App.css';
 
 function App() {
+    const [form, setForm] = useState({name: '', username: '', email: ''});
+    const [users, setUsers] = useState([]);
+    const [filteredUsers, setfilteredUsers] = useState([]);
 
-    const send = (e) =>{
-        console.log(e)
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(value => {
+                    setUsers(value)
+                    setfilteredUsers(value)
+                }
+            )
+    }, [])
+
+    const onSubmit = (e) => {
         e.preventDefault();
 
+        let arr = [...users];
 
+        if (form.name) {
+            arr = arr.filter(user => user.name.toLowerCase().includes(form.name.toLowerCase()));
+        }
+        if (form.username) {
+            arr = arr.filter(user => user.username.toLowerCase().includes(form.username.toLowerCase()));
+        }
+        if (form.email) {
+            arr = arr.filter(user => user.email.toLowerCase().includes(form.email.toLowerCase()));
+        }
+        setfilteredUsers(arr);
     }
 
-
+    const objFromFormHandler = (e) => {
+        const data = {...form, [e.target.name]: e.target.value}
+        setForm({...data});
+    }
 
     return (
         <div className="App">
-            <form onSubmit={send}>
+            <form>
                 <div>
-                    <label>Name: <input type='text' name={'name'}/></label>
+                    <label>Name: <input type='text' name={'name'} value={form.name}
+                                        onChange={objFromFormHandler}/></label>
+                    <label>Username: <input type='text' name={'username'} value={form.username}
+                                            onChange={objFromFormHandler}/></label>
+                    <label>Email: <input type='text' name={'email'} value={form.email}
+                                         onChange={objFromFormHandler}/></label>
                 </div>
-                <button>Send</button>
+                <button onClick={onSubmit}>Send</button>
             </form>
-
+            <Users users={filteredUsers}/>
         </div>
     );
 }
---
+++ src/components/Users/Users.js
@@ -0,0 +1,12 @@
+import User from "../User/User";
+
+const Users = ({users}) => {
+
+    return (
+        <div>
+            {users && users.map(value => <User key={value.id} user={value}/>)}
+        </div>
+    );
+};
+
+export default Users;
