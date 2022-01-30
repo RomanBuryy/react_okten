c6d1608e203647653056581d1fc048dcbb63aacb
RomanBuryy <roman.buriy@gmail.com>
2022-01-30 15:02:56 +0200

    add lesson_3 homework+classwork

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Form from "./components/Form/Form";
+import {useState} from "react";
+import Cars from "./components/Cars/Cars";
+
 
 function App() {
+    const [cars, setCars] = useState([]);
+    
+    const getFormData = (data) => {
+      setCars([...cars, {id: new Date().getTime(), ...data}])
+    }
+    
+    const getCarId = (id) => {
+      setCars(cars.filter(car => car.id !== id))
+    }
+    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <Form getFormData={getFormData}/>
+            <Cars cars={cars} getCarId={getCarId}/>
     </div>
   );
 }
