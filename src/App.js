72bc4d5ef17650ed084a7d57821cd2ed2f2464dd
RomanBuryy <roman.buriy@gmail.com>
2022-02-01 19:25:34 +0200

    add lesson_5 homework

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
 import Form from "./components/Form/Form";
-import {useState} from "react";
 import Cars from "./components/Cars/Cars";
 
-
 function App() {
-
-
 
     return (
         <div className="App">
